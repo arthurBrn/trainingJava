c1e6817b61e0c05d474bc6bad53e71c749f370f4
baronHost <arthur.brn333@gmail.com>
2018-09-18 14:59:40 +0200

    Modification NameSeparator.java. Creation of method conformity. Documentation added. class cleaned and finalize.

--- repoTrainingJava/src/modele/NameSeparator.java
+++ repoTrainingJava/src/modele/NameSeparator.java
@@ -1,8 +1,7 @@
 /*
  * # Class : NameSeparator
- * # Purpos : exercise on String - Take a name, split it in two and return it
- * 
- * # Left to do : Turn that class into a method which will take a String and return one
+ * # Purpos : exercise on String - The user enter a name, which is processed by the conformity method which decide wether or not the name is conform
+ * 			  The method return the boolean 'outcome'
  * */
 package modele;
 
@@ -11,35 +10,49 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class NameSeparator {
+	public static boolean conformity(String fullName) {
+		//Declaration of three String variable
+		String firstName , lastName;
+		boolean outcome=false;
+		
+		/*
+		 * If fullName is either empty nor null set outcome as false. End the program there.
+		 * */
+		if(fullName.isEmpty() || fullName==null) {
+			outcome=false;
+		}
+		else {
+			//We create a new instance of StringTokenizer named 'st', which will separate the user input as soon as there is a blank " " detected.
+			StringTokenizer st = new StringTokenizer(fullName, " ");
+			firstName = st.nextToken();//We place the first part of the user input in 'Firstname' thanks to to the nextToekn() method
+			lastName = st.nextToken();//We place the second part of the user input in 'LastName' thanks to the nextToken() method 
 
+			/*
+			 * We create condition if with two regex which are the same and would probably be more practicable through the forme of a single pattern.
+			 * [A-Za-z]* --> mean that only the letter between 'a' and 'z' are accepted, maj or min, the '*' mean that there can be several of them
+			 * If those condition are true we set 'outcome' as true 
+			 * Else we set 'outcome' as false 
+			 * */
+			if(firstName.matches("[A-Za-z]*") && lastName.matches("[A-Za-z]*")) {
+				outcome=true;
+			}
+			else {
+				outcome=false;
+			}	
+		}
+		return outcome; //We return outcome
+	}
 
 	public static void main(String[] args) throws IOException {
-		/* Scanner sc = new Scanner(System.in);
-	    String firstName="";
-	    String lastName="";
-	    String fullName="";
-	    String names[] = new String[2];
+		String name;
+		//We create a new instance of the Scanner object named 'output', which will allow us to recover the user input
+		Scanner output = new Scanner(System.in);
+		System.out.println("Enter your full name : ");
+		name = output.nextLine(); //We place the user input inside the String variable 'name'
+		//We display the outcome of the user input after being pass through the conformity method
+		System.out.println("Your String is conform : " + conformity(name));
 
-	    System.out.println("Entrez votre nom et prénom :");
-	    fullName = sc.nextLine();
-	    names = fullName.split(" ");
-	    firstName = names[0];
-	    lastName = names[1];
-	    System.out.println("Full name: "+fullName+" First name :"+firstName+" Last Name :"+lastName);
 
-	}*/
-
-		String Fullname , Firstname , Lastname;
-
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter Full Name");
-		Fullname = input.nextLine();
-
-		StringTokenizer st = new StringTokenizer(Fullname, " ");
-		Firstname = st.nextToken();
-		Lastname = st.nextToken();
-		System.out.println("First Name: "+Firstname);
-		System.out.println("Last Name: "+Lastname);
 	}
-	
+
 }
